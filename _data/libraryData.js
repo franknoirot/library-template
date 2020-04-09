33313bc65d87fbfaf3f80851af6910d1c6d3e49c
Frank Johnson <frank@franknoirot.co>
2020-04-09 14:33:20 -0400

    added live preview page!

--- _data/libraryData.js
+++ _data/libraryData.js
@@ -1,13 +1,17 @@
 const Tabletop = require('tabletop')
+const dotenv = require('dotenv')
 
 module.exports = async function () {
+    console.log('process.env = ', process.env.GOOGLE_SHEET_URL)
     let publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1L6pFNR2fB9451zNvaNzXW_tFJ2ko7YqvuD8qmNz0NWk/edit?usp=sharing';
 
     async function init() {
         return Tabletop.init({ key: publicSpreadsheetUrl,
                         }).then((data, tabletop) => {
-                            console.log('got the data!', data['Books'].elements)
-                            return data['Books'].elements
+                            return { 
+                                siteData: data['Site Data'].elements,
+                                books: data['Books'].elements,
+                            }
                         })
     }
 
